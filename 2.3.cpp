4fd69e4e6194e0a20a147cbfc0618853ef8d3b2c
Eaglorn <lexus1.45@mail.ru>
2023-02-21 22:35:03 +1000

    update

---
+++ 2.3.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    long login,password,login1,login2,password1,password2;
+    
+    login1 = 11111;
+    password1 = 22222;
+    
+    login2 = 33333;
+    password2 = 44444;
+    
+    printf("Введите логин: ");
+    scanf("%i", &login);
+    
+    printf("Введите пароль: ");
+    scanf("%i", &password);
+	
+	if((login==login1)&&(password==password1))
+	{
+		printf("Выполнена авторизация под пользователем 11111");
+	}
+	else
+	{
+		if ((login==login2)&&(password==password2))
+		{
+			printf("Выполнена авторизация под пользователем 33333");
+		}
+		else
+		{
+			printf("Не верно введен логин или пароль");
+		}
+	}
+}
