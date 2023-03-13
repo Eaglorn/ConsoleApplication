91c72b5b75fc949f93e4ff554708a28d4d678ea7
Eaglorn <lexus1.45@mail.ru>
2023-03-13 22:14:51 +1000

    Create 4.2.cpp

---
+++ 4.2.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int mas[6];
+    
+    for (int i = 0; i < 7; i++)
+    {
+    	mas[i]=rand();
+        printf("%i \n",mas[i]);
+    }
+    
+}
