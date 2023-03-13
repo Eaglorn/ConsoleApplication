6464955fa9e9bb2bf4f3f6d4f7f22b58d749ef35
Eaglorn <lexus1.45@mail.ru>
2023-03-13 22:23:45 +1000

    Create 5.2.cpp

---
+++ 5.2.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int row,column;
+    
+    printf("Количество строк=");
+    scanf("%i", &row);
+    
+    printf("Количество столбцов=");
+    scanf("%i", &column);
+    
+    row=row-1;
+    column=column-1;
+	
+	int mas[row][column];
+    
+    for (int j = 0; j <= row; j++)
+    {
+    	for (int k = 0; k <= column; k++)
+    	{
+    		scanf("%i", &mas[j][k]);
+    	}
+    }
+    
+}
