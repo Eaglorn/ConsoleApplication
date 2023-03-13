1a3e6e41b1b11204ddfef3b53344b110b1099c34
Eaglorn <lexus1.45@mail.ru>
2023-03-13 22:32:18 +1000

    Create 7.2.cpp

---
+++ 7.2.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+void mas(int arg[], int count)
+{
+	for (int i = 0; i < count; i++)
+    {
+    	arg[i]=rand();
+    	printf("%i ", arg[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+	int row,column;
+	
+	int mas1[5];
+	int mas2[10];
+	
+	mas(mas1,5);
+	mas(mas2,10);
+    
+}
+
+
